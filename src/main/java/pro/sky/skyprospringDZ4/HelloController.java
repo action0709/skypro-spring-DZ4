6f4304a5e5bae632943b49b1b7b851b806123a0d
action0709 <action0709@mail.ru>
2023-05-05 19:28:28 +0400

    1

---
+++ src/main/java/pro/sky/skyprospringDZ4/HelloController.java
@@ -0,0 +1,7 @@
+package pro.sky.skyprospringDZ4;
+
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HelloController {
+}
---
+++ src/main/java/pro/sky/skyprospringDZ4/SkyproSpringDz4Application.java
@@ -0,0 +1,13 @@
+package pro.sky.skyprospringDZ4;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SkyproSpringDz4Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SkyproSpringDz4Application.class, args);
+	}
+
+}
